30e7bee7f5238b9cafe74a4d703cf0f12baf9c2a
Lily Springer <lilyspringer@hotmail.com>
2023-02-16 13:31:02 -0700

    steps 1-4 complete

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const addMovie = event => {
+    event.preventDefault()
     let inputField = document.querySelector('input')
-
+    
     let movie = document.createElement('li')
 
+    let movieTitle = document.createElement('span')
+    movieTitle.textContent = inputField.value
     
+    movieTitle.addEventListener('click', crossOffMovie)
+
+    movie.appendChild(movieTitle)
+
+    let deleteBtn = document.createElement('button')
+    deleteBtn.textContent = 'X'
+    deleteBtn.addEventListener('click', deleteMovie)
+
+    movie.appendChild(deleteBtn)
+
+    document.querySelector('ul').appendChild(movie)
+    
+    inputField.value = ''
 }
 
+const deleteMovie = event => {
+    event.target.parentNode.remove()
+}
+
+const crossOffMovie = event => {
+    event.target.classList.toggle('checked')
+}
+
+document.querySelector('form').addEventListener('submit', addMovie)
